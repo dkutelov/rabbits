2a207642ab0f3f4963e6c54b972461ded6bb8c53
dkutelov <dkutelov@digitals.bg>
2021-03-18 15:27:39 +0200

    question form controlled fields - fixed 5 questions

--- components/create-question-form/answer.component.js
+++ components/create-question-form/answer.component.js
@@ -1,4 +1,10 @@
-export default function Answer({ count }) {
+export default function Answer({
+  count,
+  handleInputChange,
+  handleMultipleSelectChange,
+  currentValues,
+}) {
+  console.log(currentValues);
   return (
     <div className="mb-8 border-t pt-4">
       <div className="flex-grow">
@@ -8,72 +14,67 @@ export default function Answer({ count }) {
         <input
           className="border border-gray-300 shadow p-3 w-full rounded"
           id="name"
+          name={`answer${count}`}
+          value={
+            currentValues[`answer${count}`]
+              ? currentValues[`answer${count}`]
+              : ""
+          }
+          onChange={handleInputChange}
           type="text"
-          placeholder="Правя ..."
+          placeholder={`Текст на отговор ${count}`}
         />
       </div>
-      <div className="grid grid-cols-6 gap-6 mt-4">
-        <div className="col-span-6 sm:col-span-3">
+      <div className="w-full mt-4">
+        <div>
           <label
-            htmlFor="char1"
+            htmlFor={`characteristics-${count}`}
             className="block text-sm font-medium text-gray-700"
           >
             Характеристики
           </label>
           <select
-            id="char1"
-            name="char1"
-            autoComplete=">Личностови характеристики 1"
-            className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            multiple
+            value={
+              currentValues[`answer-${count}-characteristics`]
+                ? currentValues[`answer-${count}-characteristics`]
+                : ["0"]
+            }
+            onChange={handleMultipleSelectChange}
+            id={`characteristics-${count}`}
+            name={`answer-${count}-characteristics`}
+            autoComplete="Избери характеристика"
+            className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm md:text-base h-48"
           >
+            <option value="0">Избери характеристика</option>
             <optgroup label="Личностови">
-              <option>Личностови характеристики 1</option>
-              <option>Личностови характеристики 2</option>
-              <option>Личностови характеристики 3</option>
-              <option>Личностови характеристики 4</option>
-              <option>Личностови характеристики 5</option>
-              <option>Личностови характеристики 6</option>
-              <option>Личностови характеристики 7</option>
-              <option>Личностови характеристики 8</option>
-              <option>Личностови характеристики 9</option>
-              <option>Личностови характеристики 10</option>
-              <option>Личностови характеристики 11</option>
-              <option>Личностови характеристики 12</option>
-              <option>Личностови характеристики 13</option>
+              <option value="1">Шизоидна</option>
+              <option value="2">Шизотипна</option>
+              <option value="3">Параноидна</option>
+              <option value="4">Гранична</option>
+              <option value="5">Антисоциална</option>
+              <option value="6">Хистрионна</option>
+              <option value="7">Нарцистична</option>
+              <option value="8">Отбягваща</option>
+              <option value="9">Зависима</option>
+              <option value="10">Депресивна</option>
+              <option value="11">Обсесивно-компулсивна</option>
+              <option value="12">Пасивно-агресивна</option>
             </optgroup>
-            <optgroup label="Лъжа">
-              <option>Да</option>
-              <option>Не</option>
-            </optgroup>
-          </select>
-        </div>
-        <div className="col-span-6 sm:col-span-3">
-          <label
-            htmlFor="country"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Други характеристики
-          </label>
-          <select
-            id="country"
-            name="country"
-            autoComplete="country"
-            className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
             <optgroup label="Темперамент">
-              <option>Темперамент 1</option>
-              <option>Темперамент 2</option>
-              <option>Темперамент 3</option>
-              <option>Темперамент 4</option>
+              <option value="13">Пасивно отбягване</option>
+              <option value="14">Увеличена активация</option>
+              <option value="15">Социална желателност</option>
+              <option value="16">Частично подкрепяне</option>
             </optgroup>
             <optgroup label="Характер">
-              <option>Характер 1</option>
-              <option>Характер 2</option>
-              <option>Характер 3</option>
+              <option value="17">Самоуправление</option>
+              <option value="18">Кооперативност</option>
+              <option value="19">Интуитивност</option>
             </optgroup>
             <optgroup label="Тип функциониране">
-              <option>Тип функциониране 1</option>
-              <option>Тип функциониране 2</option>
+              <option>Художествен</option>
+              <option>Мисловен</option>
             </optgroup>
           </select>
         </div>
--- components/create-question-form/question-form.component.js
+++ components/create-question-form/question-form.component.js
@@ -1,52 +1,110 @@
+import { Component } from "react";
 import Answer from "./answer.component";
 
-export default function QuestionForm() {
-  return (
-    <div className="bg-blue-200 py-12 px-10 min-h-screen">
-      <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
-        <h3 className="text-center text-blue-400 font-bold mb-8 text-xl">
-          Създай Нов Въпрос
-        </h3>
-        <form>
-          <div className="mb-5">
-            <label
-              htmlFor="name"
-              className="block mb-2 font-bold text-gray-600"
-            >
-              Въпрос
-            </label>
-            <input
-              className="border border-gray-300 shadow p-3 w-full rounded"
-              id="name"
-              type="text"
-              placeholder="Какво правите когато вашия шеф ...?"
-            />
-          </div>
-          <div className="mb-8">
-            <label
-              htmlFor="question-type"
-              className="block mb-2 font-bold text-gray-600"
-            >
-              Вид въпрос
-            </label>
-            <select
-              className="border border-gray-300 shadow p-3 w-full rounded"
-              id="question-type"
-              type="text"
-            >
-              <option value="1">Един отговор</option>
-              <option value="2">Множестени отговори</option>
-              <option value="3">Въпрос Лъжа</option>
-            </select>
-          </div>
-          {Array.from({ length: 5 }, () => {}).map((_, i) => (
-            <Answer key={i} count={i + 1} />
-          ))}
-          <button className="block w-full bg-blue-500 text-blue-100 rounded-lg p-4 font-bold">
-            Създай
-          </button>
-        </form>
+export default class QuestionForm extends Component {
+  state = {
+    question: "",
+    type: "0",
+    relatedQuestionId: "",
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleMultipleSelectChange = (e) => {
+    let value = Array.from(e.target.selectedOptions, (option) => option.value);
+    this.setState({ [e.target.name]: value });
+  };
+
+  render() {
+    return (
+      <div className="bg-blue-200 py-12 px-10 min-h-screen">
+        <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
+          <h3 className="text-center text-blue-400 font-bold mb-8 text-xl">
+            Създай Нов Въпрос
+          </h3>
+          <form>
+            <div className="mb-5">
+              <label
+                htmlFor="question"
+                className="block mb-2 font-bold text-gray-600"
+              >
+                Въпрос
+              </label>
+              <input
+                className="border border-gray-300 shadow p-3 w-full rounded"
+                id="question"
+                name="question"
+                value={this.state.question}
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="Текст на въпроса"
+              />
+            </div>
+            <div className="mb-8">
+              <label
+                htmlFor="question-type"
+                className="block mb-2 font-bold text-gray-600"
+              >
+                Тип въпрос
+              </label>
+              <select
+                className="border border-gray-300 shadow p-3 w-full rounded"
+                id="question-type"
+                name="type"
+                value={this.state.type}
+                onChange={this.handleInputChange}
+                type="text"
+              >
+                <option value="0">Избор тип въпрос</option>
+                <option value="1">Един отговор</option>
+                <option value="2">Множестени отговори</option>
+                <option value="3">Въпрос Лъжа</option>
+              </select>
+            </div>
+            {this.state.type === "3" && (
+              <div className="mb-8">
+                <label
+                  htmlFor="related-question"
+                  className="block mb-2 font-bold text-gray-600"
+                >
+                  Свързан въпрос
+                </label>
+                <select
+                  className="border border-gray-300 shadow p-3 w-full rounded"
+                  id="related-question"
+                  name="relatedQuestionId"
+                  value={this.state.relatedQuestion}
+                  onChange={this.handleInputChange}
+                  type="text"
+                >
+                  <option value="0">Избор свързан въпрос</option>
+                  <option value="1">Въпрос 1</option>
+                  <option value="2">Въпрос 2</option>
+                  <option value="3">Въпрос 3</option>
+                  <option value="4">Въпрос 4</option>
+                  <option value="5">Въпрос 5</option>
+                </select>
+              </div>
+            )}
+            {Array.from({ length: 5 }, () => {}).map((_, i) => (
+              <Answer
+                key={i}
+                count={i + 1}
+                handleInputChange={this.handleInputChange}
+                handleMultipleSelectChange={this.handleMultipleSelectChange}
+                currentValues={this.state}
+              />
+            ))}
+            <button className="block w-full bg-blue-500 text-blue-100 rounded-lg p-4 font-bold">
+              Създай
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
